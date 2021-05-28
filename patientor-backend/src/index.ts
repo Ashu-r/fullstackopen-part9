50cf447043f7ccd6f7e0f51a30d342efbe059849
Ashu-r <randiveashutosh@gmail.com>
2021-05-28 21:30:32 +0530

    9.10: Patientor backend, step3

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT = 3001;
 
+app.use('/api/diagnoses', diagnoseRouter);
 app.get('/api/ping', (_req, res) => {
 	console.log('someone pinged here');
 	res.send('pong');
